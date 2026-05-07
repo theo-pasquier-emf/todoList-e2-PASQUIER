8df428908bf0550e290e5cadcd41e87626c30dba
Pasquier Théo <theo.pasquier@studentfr.ch>
2026-05-07 13:39:25 +0200

    cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
 
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
